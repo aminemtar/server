9ffefb3675cf13260d190ccca420d169dfd21944
Mohamed Amine Mtar <mohamedamine.mtar@esprit.tn>
2022-12-06 17:39:55 +0100

    deploi

--- back/controllers/Authcontroller.js
+++ back/controllers/Authcontroller.js
@@ -754,8 +754,10 @@ export async function confirmAccount(email) {
                           <table border="0" cellpadding="0" cellspacing="0">
                             <tr>
                               <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                                <a href='https://cicero.crm.com/api/confirm/`+email+`' target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Comfirm your email</a>
-                              </td>
+                                <a href='https://cicero-crm.com/api/confirm/`+email+`' target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Comfirm your email</a>
+                                <a href='https://cicero-crm.com/api/confirm/`+email+`' target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Comfirm your email</a>
+
+                                </td>
                             </tr>
                           </table>
                         </td>
